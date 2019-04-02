627dc055df2a6f989705b934040baa5b4c49046b
khaisoe <khaingsoe@pursuit.org>
2019-04-01 21:40:17 -0400

    migrated to androidx and added menus

--- app/src/main/java/com/pursuit/letskeepintouch/fragments/SplashScreenFragment.java
+++ app/src/main/java/com/pursuit/letskeepintouch/fragments/SplashScreenFragment.java
@@ -1,13 +1,11 @@
 package com.pursuit.letskeepintouch.fragments;
 
 import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -57,15 +55,36 @@ public class SplashScreenFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                fragmentInterface.moveToScanningFragment();
+       /* requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.fragment_splash_screen);
+        getSupportActionBar();
+        LogoLauncher logoLauncher = new LogoLauncher();
+        logoLauncher.start();
+    }
+
+    private class LogoLauncher extends Thread {
+        public void run() {
+
+            try {
+                sleep(1300);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-        }, SCREEN_TIME);
+           fragmentInterface.moveToScanningFragment();
+        }
+    }
+*/
+
+//        new Handler().postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                fragmentInterface.moveToScanningFragment();
+//            }
+//        }, SCREEN_TIME);
 
 
-}
+    }
 
 
 
